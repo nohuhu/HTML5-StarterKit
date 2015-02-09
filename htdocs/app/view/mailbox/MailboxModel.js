387866f9ec4ed5ab9637e339c7ffbda8fef8c228
Nimloth <martin@Martins-Mac-Pro.fritz.box>
2015-02-09 20:13:22 +0100

    Added one view & controller per grid
    Customers, Domains, Mailboxes now each have their own View and
    ViewController.
    Also added a pagingtoolbar.

---
+++ htdocs/app/view/mailbox/MailboxModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('StarterKit.view.mailbox.MailboxModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.mailbox',
+
+    data: {
+        name: 'StarterKit'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
