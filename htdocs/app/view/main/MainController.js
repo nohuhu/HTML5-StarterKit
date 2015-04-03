afe75c90a0e7e8ffff100ef4d3e8d7d12616613e
Martin Waschbüsch <martin@waschbuesch.de>
2015-04-03 16:13:06 +0200

    removed leftover functions in MainController
    Since these methods where moved to CrudController, they are no longer
    needed here?

--- htdocs/app/view/main/MainController.js
+++ htdocs/app/view/main/MainController.js
@@ -26,36 +26,6 @@ Ext.define('StarterKit.view.main.MainController', {
     onClickButton: function () {
         Ext.Msg.confirm('Confirm', 'Are you sure?', 'onConfirm', this);
     },
-
-    onConfirm: function (choice) {
-        if (choice === 'yes') {
-            //
-        }
-    },
-    
-    onAdd: function(button) {
-        var grid, store, Model, plugin, record;
-        
-        grid   = this.lookupReference(button.gridReference);
-        store  = grid.getStore();
-        Model  = store.getModel();
-        plugin = grid.getPlugin('editor');
-        
-        record = new Model();
-        store.add(record);
-        plugin.startEdit(record);
-    },
-    
-    onDelete: function(button) {
-        var grid, selection, i, len;
-        
-        grid = this.lookupReference(button.gridReference);
-        selection = grid.getSelectionModel().getSelection();
-        
-        for (i = 0, len = selection.length; i < len; i++) {
-            grid.getStore().remove(selection[i]);
-        }
-    },
     
     onLoginSuccess: function(data) {
         var last_login = Ext.Date.parse(data.last_login, 'U');
